aa3180bfdf9ba3182e706940a8aadcea025aa0e0
haturatu <taro@eyes4you,org>
2023-09-14 10:55:09 +0900

    Initial commit

---
+++ innertube/src/main/kotlin/it/vfsfitvnm/innertube/models/Context.kt
@@ -0,0 +1,60 @@
+package it.vfsfitvnm.innertube.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Context(
+    val client: Client,
+    val thirdParty: ThirdParty? = null,
+) {
+    @Serializable
+    data class Client(
+        val clientName: String,
+        val clientVersion: String,
+        val platform: String,
+        val hl: String,
+        val gl: String,
+        val visitorData: String = "CgtEUlRINDFjdm1YayjX1pSaBg%3D%3D",
+        val androidSdkVersion: Int? = null,
+        val userAgent: String? = null
+    )
+
+    @Serializable
+    data class ThirdParty(
+        val embedUrl: String,
+    )
+
+    companion object {
+        val DefaultWeb = Context(
+            client = Client(
+                clientName = "WEB_REMIX",
+                clientVersion = "1.20220918",
+                platform = "DESKTOP",
+                hl = "en",
+                gl = "JP"
+            )
+        )
+
+        val DefaultAndroid = Context(
+            client = Client(
+                clientName = "ANDROID_MUSIC",
+                clientVersion = "5.28.1",
+                platform = "MOBILE",
+                androidSdkVersion = 30,
+                userAgent = "com.google.android.apps.youtube.music/5.28.1 (Linux; U; Android 11) gzip",
+                hl = "ja",
+                gl = "JP"
+            )
+        )
+
+        val DefaultAgeRestrictionBypass = Context(
+            client = Client(
+                clientName = "TVHTML5_SIMPLY_EMBEDDED_PLAYER",
+                clientVersion = "2.0",
+                platform = "TV",
+                hl = "ja",
+                gl = "JP"
+            )
+        )
+    }
+}
